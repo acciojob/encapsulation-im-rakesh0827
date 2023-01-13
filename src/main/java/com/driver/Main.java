fd7ae948bdf25ff6402dcef5a4e26c1f9542ae6c
Rakesh Kumar <acousticrakesh0827@gmail.com>
2023-01-13 18:19:21 +0530

    Quick Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,8 @@ public class Main{
     public static void main(String[] args) {
        RWOnly ojb = new RWOnly();
        obj.setName("Rakesh");
-       String userName = obj.getName();
-       System.out.println(userName);
+
+
     }
-    
+
 }
